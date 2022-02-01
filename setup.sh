869ac697264a2c67ec381ebd72c077e0366535a2
dubuisa <antoine.dubuis@gmail.com>
2022-02-01 12:32:30 +0100

    fix theme

--- setup.sh
+++ setup.sh
@@ -1,11 +1,11 @@
 mkdir -p ~/.streamlit/
 
 echo "[theme]
-primaryColor=‘#1fc857‘
-backgroundColor=‘#100f21‘
-secondaryBackgroundColor=‘#1e1d32‘
-textColor=‘#ffffff‘
-font=‘sans serif‘
+primaryColor='#1fc857'
+backgroundColor='#100f21'
+secondaryBackgroundColor='#1e1d32'
+textColor='#ffffff'
+font='sans serif'
 [server]
 headless=true
 enableCORS=false
